da34bc387b45aea94ee7532943e8432b4ef4f58e
Syed Rafsan Raiyan <srafsan98@tutanota.com>
2023-03-07 07:27:31 +0600

    Fixed all of the deposit errors

--- js/deposit.js
+++ js/deposit.js
@@ -10,16 +10,18 @@ document.getElementById("btn-deposit").addEventListener("click", function () {
     1. get previous deposit total by id
 
     */
-    const previousDepositTotal = getTextElementValueById("deposit-total");
+    const previousDepositTotal =
+        document.getElementById("deposit-total").innerText;
 
     // calculate new deposit total
-    const newDepositTotal = previousDepositTotal + newDepositAmount;
+    const newDepositTotal =
+        parseInt(previousDepositTotal) + parseInt(newDepositAmount);
     console.log(newDepositTotal, previousDepositTotal, newDepositAmount);
     // set deposit total value
     setTextElementValueById("deposit-total", newDepositTotal);
 
     // get previous balance by using the function
-    const previousBalanceTotal = getElementValueById("balance-total");
+    const previousBalanceTotal = document.getElementById("balance-total");
     const newBalanceTotal = previousBalanceTotal + newDepositAmount;
     setTextElementValueById("balance-total", newBalanceTotal);
 });
